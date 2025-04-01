d715b4ed9347d1bb6cde4d96b97f991c862de157
Abhinav <abhinavsunil@hotmail.com>
2025-04-01 22:04:38 +0530

    maded some improveemnts

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/basics.kt
@@ -7,8 +7,11 @@ fun main()
     number+=10
     //Val cannot be reassigned.
     //var can be reassigned.
-
-
+    val num : Byte = 40
+    val num1 :Int = 45896
+    val num2 :Long = 147841223254877951
+    println(num2)
+    println(num)
     println(number)
     println(myname)
     println("Hello Students")
