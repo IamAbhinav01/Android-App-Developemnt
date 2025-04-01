d124307e432c2bff680dd07c31f8297d9830b0cd
Abhinav <abhinavsunil@hotmail.com>
2025-04-01 22:00:51 +0530

    Added the basics for kotlin , along with types of varibales and integr types

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/basics.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbasics
+
+fun main()
+{
+    val myname = "Abhinav"
+    var number = 10
+    number+=10
+    //Val cannot be reassigned.
+    //var can be reassigned.
+
+
+    println(number)
+    println(myname)
+    println("Hello Students")
+}
