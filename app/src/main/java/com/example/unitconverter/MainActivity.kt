1e3a5c8fb395a8626235b1517ef07fab732eef3b
Abhinav <abhinavsunil@hotmail.com>
2025-04-16 19:56:30 +0530

    modifed the code and added textField

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,21 +33,21 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverter(){
     Column {
-        //Greeting("Abhinav")
-        //Greeting("Sunil")
+        Text("Unit Converter")
+        OutlinedTextField(value = " ", onValueChange = {})
         Row {
-            Greeting("Abhinav")
-            Greeting("Sunil")
+
         }
     }
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!    ",
+        text = "$name ",
         modifier = modifier
     )
 }
